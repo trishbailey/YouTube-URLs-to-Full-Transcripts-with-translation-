dc668940c3cc91062fad5443eae8704d95b4aecf
Trish <trishbailey321@gmail.com>
2026-03-09 06:18:51 -0400

    Import additional exceptions from youtube_transcript_api

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import io
 import re
-from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound
 from youtube_transcript_api._errors import TranscriptsDisabled, NoTranscriptFound
 from deep_translator import GoogleTranslator
 from openpyxl import Workbook
